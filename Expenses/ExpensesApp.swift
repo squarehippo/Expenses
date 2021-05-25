925a0ba3b1a5198ca5ad91f430693c2277e81bc1
Brian Wilson <squarehippobills@gmail.com>
2021-05-25 15:49:27 -0400

    Initial Commit

---
+++ Expenses/ExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensesApp.swift
+//  Expenses
+//
+//  Created by Brian Wilson on 5/25/21.
+//
+
+import SwiftUI
+
+@main
+struct ExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
